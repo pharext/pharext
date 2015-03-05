3dfe0bf2f02bbcf39a3b137913823c62fa03b694
Michael Wallner <mike@php.net>
2015-03-05 09:09:28 +0100

    cleanup

--- src/pharext_packager.php
+++ src/pharext_packager.php
@@ -1,16 +1,11 @@
 <?php
-
 /**
- * The packager stub for bin/pharext
+ * The packager sub-stub for bin/pharext
  */
 
-#Phar::mapPhar("pharext.phar");
-
 function __autoload($c) {
-	return include /*"phar://pharext.phar/".*/strtr($c, "\\_", "//") . ".php";
+	return include strtr($c, "\\_", "//") . ".php";
 }
 
 $packager = new pharext\Packager();
 $packager->run($argc, $argv);
-
-__HALT_COMPILER();
